0ba6b63a1c1cb5133076337b108efb20aa795169
ashuhlee <ashleyoye24@gmail.com>
2026-02-03 11:45:11 -0500

    style: glitch effects

---
+++ src/renderer/scripts/anim/glitchEffect.js
@@ -0,0 +1,48 @@
+
+const PowerGlitch = require('powerglitch').PowerGlitch;
+
+export function createGlitch(element, duration, iterations) {
+
+	PowerGlitch.glitch(element, {
+		timing: {
+			duration: duration,
+			iterations: iterations,
+			easing: 'linear'
+		},
+		shake: {
+			velocity: 10,
+		},
+		slice: {
+			count: 3,
+			hueRotate: false
+		},
+	});
+}
+
+export function heartGlitch() {
+	const hearts = ['.heart4', '.heart2', '.heart1', '.heart3'];
+	const delay = 400;
+
+	hearts.forEach((heartItem, index) => {
+		setTimeout(() => {
+			PowerGlitch.glitch(heartItem, {
+				timing: {
+					duration: 2500,
+					easing: 'ease',
+					iterations: 1
+				},
+				glitchTimeSpan: {
+					start: 0,
+					end: 0.16,
+				},
+				shake: {
+					velocity: 1,
+				},
+				slice: {
+					count: 2,
+					hueRotate: false
+				}
+			});
+		}, index * delay)
+	});
+}
