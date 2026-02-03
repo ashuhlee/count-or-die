b08e064962ef8a9bc698fb6a631c824d3837cdf5
ashuhlee <ashleyoye24@gmail.com>
2026-02-02 23:22:37 -0500

    style: button presses + text effects

--- src/renderer/scripts/anim/animations.js
+++ src/renderer/scripts/anim/animations.js
@@ -1,5 +1,43 @@
 
+let i = 0;
 const animClasses = ["pop", "pop-dec", "reset-shake", "new-goal"];
+
+export const btnImages = {
+	increase: {
+		normal: "../assets/images/png/buttons/increase.png",
+		pressed: "../assets/images/png/buttons/increase-press.png",
+	},
+	decrease: {
+		normal: "../assets/images/png/buttons/decrease.png",
+		pressed: "../assets/images/png/buttons/decrease-press.png",
+	},
+};
+
+
+function updateColor() {
+	const colors = ['#e68ad6', '#A193FF', '#94BEFF', '#BEB9DC', "#FFB7A4"];
+	const color = colors[i];
+	i = (i + 1) % colors.length;
+
+	return color;
+}
+
+// match goal box colors with the letters
+function applyColorTheme(color) {
+
+	const goalBox = document.querySelector(".next-goal");
+	goalBox.style.borderTop = `3px solid ${color}`;
+
+	// goalBox.style.borderTop = `4px solid ${color}`;
+	// goalBox.style.borderLeft = `4px solid ${color}`;
+	// goalBox.style.boxShadow = `-4px 0 0 0 ${color}, 4px 0 0 0 ${color}, 0 -4px 0 0 ${color}`
+
+	const chatDeco = document.querySelector(".chat-deco");
+	chatDeco.style.fill = `${color}`;
+
+	// const goalChatBox = document.querySelector(".goal-chatbox");
+	// goalChatBox.style.filter = `drop-shadow(4px 4px 0px ${color})`;
+}
 
 // main animation function
 export function playAnimation(element, className) {
@@ -21,8 +59,23 @@ export function playAnimationMulti(elements, className) {
 	});
 }
 
+// clicking animation
+export function animateBtn(btnType) {
+
+    const img = document.getElementById(`${btnType}-img`);
+    img.src = btnImages[btnType].pressed;
+
+	setTimeout(() => {
+		img.src = btnImages[btnType].normal;
+	}, 100);
+}
+
 // show text by letter
-export function wrapLetters(className) {
+export function splitLetters(className, animationType = "fade") {
+
+	let newColor = updateColor();
+	applyColorTheme(newColor);
+
 	const elements = document.querySelectorAll(className);
 	elements.forEach(element => {
 		const text = element.textContent;
@@ -31,18 +84,16 @@ export function wrapLetters(className) {
 		text.split("").forEach((char, index) => {
 			const span = document.createElement("span");
 			span.textContent = char === " " ? "\u00A0" : char;
-			span.style.setProperty('--delay', `${index * 0.05}s`);
+
+			if (animationType === "wavy") {
+				span.style.setProperty('--delay', `${index * 0.1}s`);
+
+			} else {
+				span.style.setProperty('--delay', `${index * 0.05}s`);
+				span.style.setProperty("color", newColor);
+			}
+
 			element.appendChild(span);
 		});
-	});
-}
-
-// trigger animation
-export function playLetterAnim(element) {
-	const spans = element.querySelectorAll('span');
-	spans.forEach(span => {
-		span.style.animation = 'none';
-		span.offsetHeight;
-		span.style.animation = '';
 	});
 }
