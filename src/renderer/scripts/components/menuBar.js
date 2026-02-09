cd786b82964f0b371fc2327ead2013267fbd1f74
ashuhlee <ashleyoye24@gmail.com>
2026-02-08 19:23:31 -0500

    feat: save sound toggle settings to local storage

--- src/renderer/scripts/components/menuBar.js
+++ src/renderer/scripts/components/menuBar.js
@@ -6,10 +6,18 @@ import soundBtnUnmuted from "../../assets/ui/svg/sound-button-reg.svg";
 export function soundToggle() {
 
 	const toggleSoundBtn = document.getElementById("soundBtn");
+	const savedMutedState = localStorage.getItem("soundMuted");
+
+	if (savedMutedState !== null) {
+		sounds.bgMusic.muted = (savedMutedState === "true");
+		toggleSoundBtn.src = sounds.bgMusic.muted ? soundBtnMuted : soundBtnUnmuted;
+	}
 
 	toggleSoundBtn.addEventListener("click", () => {
-		console.log("clicked")
+		console.log(`is sound playing: ${sounds.bgMusic.muted.toString()}`);
 		toggleMusic(sounds.bgMusic);
 		toggleSoundBtn.src = sounds.bgMusic.muted ? soundBtnMuted : soundBtnUnmuted;
+
+		localStorage.setItem("soundMuted", sounds.bgMusic.muted.toString());
 	})
 }
