20159582d85492dacdc7cf5fe0f5f30b8835e96a
ashuhlee <ashleyoye24@gmail.com>
2026-02-09 06:10:43 -0500

    feat: smoke animation on boost use

--- src/renderer/scripts/components/heartDisplay.js
+++ src/renderer/scripts/components/heartDisplay.js
@@ -8,11 +8,36 @@ export function removeHeart(boostsLeft) {
 
 	if (heart) {
 		heart.classList.add("heart-use");
+		addUseEffect(boostsLeft);
 	}
 
 	if (boostsLeft === 0) {
 		noBoostsText.style.display = "grid";
 	}
+}
+
+function addUseEffect(boostsLeft) {
+	let i = boostsLeft + 1;
+
+	const target = document.getElementById(`boost-${i}`);
+	const popupElement = document.getElementById('popupElement');
+
+	const rect = target.getBoundingClientRect();
+
+	const smokeWidth = 65;
+	const smokeHeight = 65;
+
+	const xPosition = rect.left; // x position of element
+	const yPosition = rect.top;  // y position of element
+
+	console.log("X:", xPosition, "Y:", yPosition);
+
+	popupElement.style.left = `${xPosition}px`;
+	popupElement.style.top = `${(yPosition) - 18}px`;
+
+	// display smoke effect
+	popupElement.src = 'assets/ui/deco/smoke.gif?t=0';
+	popupElement.style.display = 'block';
 }
 
 export function resetHearts() {
