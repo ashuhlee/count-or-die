4568dc71b63f8c8fe1c4eed76afa45ee8c2462b6
ashuhlee <ashleyoye24@gmail.com>
2026-02-14 20:41:21 -0500

    chore: file cleanup

--- src/main/main.js
+++ src/main/main.js
@@ -1,83 +1,83 @@
 
 import { setDiscordStatus } from './discordRPC.js';
+
 const { app, BrowserWindow, nativeImage } = require('electron');
-const { shell } = require('electron/common');
 const { Menu } = require('electron/main');
 const { ipcMain } = require('electron');
 
 const path = require('path');
-const isMac = process.platform === 'darwin'
+const isMac = process.platform === 'darwin';
 
 const template = [
-  // { role: 'appMenu' }
-  ...(isMac
-    ? [{
-        label: app.name,
-        submenu: [
-          { role: 'about' },
-          { type: 'separator' },
-          { role: 'services' },
-          { type: 'separator' },
-          { role: 'hide' },
-          { role: 'hideOthers' },
-          { role: 'unhide' },
-          { type: 'separator' },
-          { role: 'quit' }
-        ]
-      }]
-    : []),
-  // { role: 'fileMenu' }
-  {
-    label: 'File',
-    submenu: [
-      isMac ? { role: 'close' } : { role: 'quit' }
-    ]
-  },
-  // { role: 'viewMenu' }
-  {
-    label: 'View',
-    submenu: [
-      { role: 'reload' },
-      { role: 'forceReload' },
-      { role: 'toggleDevTools' },
-      { type: 'separator' },
-      { role: 'resetZoom' },
-      { role: 'zoomIn' },
-      { role: 'zoomOut' },
-      { type: 'separator' },
-      { role: 'togglefullscreen' }
-    ]
-  },
-  // { role: 'windowMenu' }
-  {
-    label: 'Window',
-    submenu: [
-      { role: 'minimize' },
-      { role: 'zoom' },
-      ...(isMac
-        ? [
-            { type: 'separator' },
-            { role: 'front' },
-            { type: 'separator' },
-            { role: 'window' }
-          ]
-        : [
-            { role: 'close' }
-          ])
-    ]
-  },
-  {
-    role: 'help',
-    submenu: [
-      {
-        label: 'Learn More',
-        click: async () => {
-          const { shell } = require('electron');
-          await shell.openExternal('https://countordie.gg');
-        },
-      }
-    ]
-  }
+	// { role: 'appMenu' }
+	...(isMac
+		? [{
+			label: app.name,
+			submenu: [
+				{role: 'about'},
+				{type: 'separator'},
+				{role: 'services'},
+				{type: 'separator'},
+				{role: 'hide'},
+				{role: 'hideOthers'},
+				{role: 'unhide'},
+				{type: 'separator'},
+				{role: 'quit'}
+			]
+		}]
+		: []),
+	// { role: 'fileMenu' }
+	{
+		label: 'File',
+		submenu: [
+			isMac ? {role: 'close'} : {role: 'quit'}
+		]
+	},
+	// { role: 'viewMenu' }
+	{
+		label: 'View',
+		submenu: [
+			{role: 'reload'},
+			{role: 'forceReload'},
+			{role: 'toggleDevTools'},
+			{type: 'separator'},
+			{role: 'resetZoom'},
+			{role: 'zoomIn'},
+			{role: 'zoomOut'},
+			{type: 'separator'},
+			{role: 'togglefullscreen'}
+		]
+	},
+	// { role: 'windowMenu' }
+	{
+		label: 'Window',
+		submenu: [
+			{role: 'minimize'},
+			{role: 'zoom'},
+			...(isMac
+				? [
+					{type: 'separator'},
+					{role: 'front'},
+					{type: 'separator'},
+					{role: 'window'}
+				]
+				: [
+					{role: 'close'}
+				])
+		]
+	},
+	{
+		role: 'help',
+		submenu: [
+			{
+				label: 'Learn More',
+				click: async () => {
+					const {shell} = require('electron');
+					await shell.openExternal('https://countordie.gg');
+				},
+			}
+		]
+	}
 ]
 
 const menu = Menu.buildFromTemplate(template)
@@ -89,32 +89,32 @@ ipcMain.on('discord:update', (event, data) => {
 
 function createWindow() {
 
-    const iconPath = path.join(__dirname, '../../src/renderer/assets/icons/mac/icon.icns');
+	const iconPath = path.join(__dirname, '../../src/renderer/assets/icons/mac/icon.icns');
 
-    const mainWindow = new BrowserWindow({
-        width: 475,
-        height: 900,
-        resizable: true,
-        maximizable: true,
-        fullscreenable: true,
-        icon: iconPath,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true,
+	const mainWindow = new BrowserWindow({
+		width: 475,
+		height: 900,
+		resizable: true,
+		maximizable: true,
+		fullscreenable: true,
+		icon: iconPath,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true,
 			contextIsolation: true
-        }
-    });
+		}
+	});
 
-    if (isMac) {
-        const dockIcon = nativeImage.createFromPath(iconPath);
-        app.dock.setIcon(dockIcon);
-    }
+	if (isMac) {
+		const dockIcon = nativeImage.createFromPath(iconPath);
+		app.dock.setIcon(dockIcon);
+	}
 
-    if (!app.isPackaged) {
-        mainWindow.loadURL('http://localhost:5173');
-    } else {
-        mainWindow.loadFile(path.join(__dirname, '../renderer/main_window/index.html'));
-    }
+	if (!app.isPackaged) {
+		mainWindow.loadURL('http://localhost:5173');
+	} else {
+		mainWindow.loadFile(path.join(__dirname, '../renderer/main_window/index.html'));
+	}
 
 	// get high score after renderer loads
 	mainWindow.webContents.on('did-finish-load', () => {
@@ -123,7 +123,7 @@ function createWindow() {
 		mainWindow.webContents.executeJavaScript('localStorage.getItem("high-score")')
 			.then(highScore => {
 				const score = Number(highScore) || 0;
-				setDiscordStatus({ highScoreRPC: score });
+				setDiscordStatus({highScoreRPC: score});
 			})
 	});
 }
@@ -132,18 +132,18 @@ app.whenReady().then(() => {
 	createWindow();
 
 	app.on('activate', () => {
-    	if (BrowserWindow.getAllWindows().length === 0) {
-    		createWindow()
-    	}
-  	})
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
 })
 
 app.on('window-all-closed', () => {
 	if (!isMac) {
 		app.quit()
-  	}
+	}
 })
 
 ipcMain.on('close', () => {
-  app.quit()
+	app.quit()
 })
