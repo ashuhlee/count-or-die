d33e297a01d8ce094bb77fd677bd0b42238b91ac
ashuhlee <ashleyoye24@gmail.com>
2026-02-18 13:21:17 -0500

    feat: add game over effects

--- src/renderer/scripts/core/gameOver.js
+++ src/renderer/scripts/core/gameOver.js
@@ -1,8 +1,58 @@
 
-export function toggleGameOver(isGameOver = false) {
+import { resetTheme } from "../controls/themeHandler.js";
 
-    const gameOverScreen = document.getElementById("game-over");
-    gameOverScreen.style.display = isGameOver ? "grid" : "none";
+const phrasesRegular = [
+	"you died lolz",
+	"lol loser",
+	"aw, you died",
+	"you died :(",
+	"better luck next life..",
+	"nice try!",
+	"you didn't deserve that..",
+	"let's aim a bit higher..",
+	"sooo close!"
+]
+
+const phrasesNewScore = [
+	"new high score!",
+	"you beat your best score!",
+	"you're getting better!",
+	"oooh, a new record!"
+]
+
+function setRandomText(arr) {
+	const i = Math.floor(Math.random() * arr.length);
+	return arr[i];
+}
+
+export function toggleGameOver(isGameOver = false, highScoreReached = false) {
+
+	const flash = document.getElementById("red-flash");
+	const gameOverScreen = document.getElementById("game-over");
+
+	const displayText = document.querySelector(".go-text-random");
+
+	if (highScoreReached) {
+		displayText.textContent = setRandomText(phrasesNewScore);
+	}
+	else {
+		displayText.textContent = setRandomText(phrasesRegular);
+	}
+
+    if (isGameOver) {
+		flash.classList.add("flash");
+		flash.addEventListener("animationend", () => {
+			flash.classList.remove("flash");
+			gameOverScreen.classList.add("visible");
+
+			setTimeout(() => {
+				resetTheme()
+			}, 500);
+
+		}, {once: true})
+	} else {
+		gameOverScreen.classList.remove("visible");
+	}
 }
 
 export function youDiedConsole(finalScore) {
