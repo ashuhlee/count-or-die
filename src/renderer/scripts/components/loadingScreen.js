524d8796bf111704d2e6842a40e81eb34d9a1034
ashuhlee <ashleyoye24@gmail.com>
2026-02-19 16:51:59 -0500

    feat: add loading screen

---
+++ src/renderer/scripts/components/loadingScreen.js
@@ -0,0 +1,35 @@
+
+import skullImg from '../../assets/ui/deco/skull_purple.png';
+
+export function showLoadingScreen() {
+
+	const loading = document.createElement('div');
+	loading.id = 'loading-screen';
+	loading.className = 'loading-screen';
+
+	loading.innerHTML = `
+		<div class="loading-content">
+			<div class="diamond-wrapper">
+				<div class="diamond"></div>
+				<img src="${skullImg}" class="loading-skull" alt="skull"/>			
+			</div>
+		</div>`
+
+	document.body.appendChild(loading);
+
+	requestAnimationFrame(() => {
+		loading.classList.add('visible');
+	})
+}
+
+export function hideLoadingScreen() {
+
+	const loading = document.getElementById('loading-screen');
+	if (!loading) return;
+
+	loading.classList.add('hiding');
+
+	setTimeout(() => {
+		loading.remove();
+	}, 1000)
+}
