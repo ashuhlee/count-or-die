7a6673ed5db07001a0a6df123187fe0ea60c96c7
ashuhlee <ashleyoye24@gmail.com>
2026-02-28 05:44:21 -0500

    style: add background clouds

--- src/renderer/scripts/containers/mainContainer.js
+++ src/renderer/scripts/containers/mainContainer.js
@@ -10,10 +10,17 @@ import exclamationPng from '@assets/ui/deco/exclamation.png';
 
 import ghostPng from '@assets/ui/deco/ghost.gif';
 import skullFloor from '@assets/ui/deco/skull_floor.png';
+import skullTracker from '@assets/ui/deco/skull_grey.png';
 import tombstonePng from '@assets/ui/deco/tombstone.png';
 
 import resetBtnPng from '@assets/ui/buttons/reset-btn.png';
 import quitBtnPng from '@assets/ui/buttons/quit-btn.png';
+
+import cloudTopLeft from '@assets/ui/deco/clouds/cloud_1.png';
+import cloudBottomLeft from '@assets/ui/deco/clouds/cloud_2.png';
+import cloudTopRight from '@assets/ui/deco/clouds/cloud_3.png';
+import cloudBottomRight from '@assets/ui/deco/clouds/cloud_4.png';
+import cloudMini from '@assets/ui/deco/clouds/cloud_5.png';
 
 export function renderMain() {
 
@@ -25,7 +32,16 @@ export function renderMain() {
 		<div class="scanlines-bg"></div>
 		<div class="top-gradient"></div>
 		
-		<img class="left-deco" alt="left deco" src="${leftDecoPng}"/>
+<!--		<img class="left-deco" alt="left deco" src="${leftDecoPng}"/>-->
+		
+		<!-- clouds -->
+		<div class="clouds">
+			<img class="cloud-t-left" alt="cloud" src="${cloudTopLeft}"/>
+			<img class="cloud-b-left" alt="cloud" src="${cloudBottomLeft}"/>
+			<img class="cloud-mini" alt="cloud" src="${cloudMini}"/>
+			<img class="cloud-t-right" alt="cloud" src="${cloudTopRight}"/>
+			<img class="cloud-b-right" alt="cloud" src="${cloudBottomRight}"/>
+		</div>
 		
 		<img id="popupElement" alt="smoke" style="filter: brightness(1.03) saturate(110%)">
 		<img id="powerUpUsedElement" alt="smoke" style="filter: brightness(1.03) saturate(110%)">
@@ -43,8 +59,16 @@ export function renderMain() {
 			<div class="go-content">
 				<div id="red-overlay"></div>
 				<h2 class="you-died" id="dead-text"><span class="go-text-random">you died lolz</span></h2>
-				<h2 class="death-tracker" id="death-tracker">deaths: 0</h2>
-				
+				<div class="top-right-info">
+					<div class="tracker-container">
+						<img src="${skullTracker}" alt="skull" class="skull-tracker"/>	
+						<h2 class="death-tracker" id="death-tracker">14</h2>
+					</div>
+<!--					<div class="leaderboard-container">-->
+<!--						<img src="${skullTracker}" alt="skull" class="skull-tracker"/>	-->
+<!--						<h2 class="leaderboard">scores</h2>-->
+<!--					</div>-->
+				</div>
 				<div class="deco-icons">
 					<img src="${questionPng}" alt="question mark" class="question"/>
 					<img src="${exclamationPng}" alt="exclamation mark" class="excl"/>
