d36b5d1d6c355e99ab5b4008f60cb65f398b95f6
ashuhlee <ashleyoye24@gmail.com>
2026-03-03 00:15:39 -0500

    refactor: consolidate power up assets & improve spawn/effect handling

---
+++ src/renderer/assets/ui/power-ups/index.ts
@@ -0,0 +1,5 @@
+export { default as candy } from './candy.png';
+export { default as donut } from './donut.png';
+export { default as badDonut } from './bad_donut.png';
+export { default as star } from './star.png';
+export { default as superstar } from './superstar.png';
